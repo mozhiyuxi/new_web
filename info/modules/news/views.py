4abeff010fa23534d70baaa40a366ef9cda560ca
medo <a1373721@126.com>
2018-10-20 15:30:27 +0800

    评论，展示评论

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,7 +1,7 @@
 from flask import render_template, g, current_app, jsonify, abort, request
 
 from info import constants, db
-from info.models import News
+from info.models import News, Comment, User, CommentLike
 from info.utils.common import user_login_data
 from info.utils.response_code import RET
 from . import news_blu
@@ -43,11 +43,43 @@ def detail(news_id):
     # 点击量加一
     news_detail.clicks += 1
 
+    # 显示新闻评论
+    comments = []
+    try:
+        comments = Comment.query.filter(Comment.news_id == news_id).order_by(Comment.create_time.desc()).all()
+    except Exception as e:
+        current_app.logger.error(e)
+
+    comment_like_ids = []
+    if g.user:
+        try:
+            # 需求：查询当前用户在当前新闻里面都点赞了哪些评论
+            # 1. 查询出当前新闻的所有评论 ([COMMENT]) 取到所有的评论id  [1, 2, 3, 4, 5]
+            comment_ids = [comment.id for comment in comments]
+            # 2. 再查询当前评论中哪些评论被当前用户所点赞 （[CommentLike]）查询comment_id 在第1步的评论id列表内的所有数据 & CommentList.user_id = g.user.id
+            comment_likes = CommentLike.query.filter(CommentLike.comment_id.in_(comment_ids),
+                                                     CommentLike.user_id == g.user.id).all()
+            # 3. 取到所有被点赞的评论id 第2步查询出来是一个 [CommentLike] --> [3, 5]
+            comment_like_ids = [comment_like.comment_id for comment_like in comment_likes]  # [3, 5]
+        except Exception as e:
+            current_app.logger.error(e)
+
+    comment_dict_li = []
+    for comment in comments:
+        comment_dict = comment.to_dict()
+        # 代表没有点赞
+        comment_dict["is_like"] = False
+        # 判断当前遍历到的评论是否被当前登录用户所点赞
+        if comment.id in comment_like_ids:
+            comment_dict["is_like"] = True
+        comment_dict_li.append(comment_dict)
+
     data = {
         "user": user,
         "news_detail": news_detail.to_dict() if news_detail else None,
         "rank_news": news_dict_li if news_dict_li else None,
-        "is_collected": is_collected
+        "is_collected": is_collected,
+        "comments": comment_dict_li,
     }
     return render_template("news/detail.html", data=data)
 
@@ -96,3 +128,65 @@ def news_collect():
     #     db.session.rollback()
     #     return jsonify(errno=RET.DBERR, errmsg="保存失败")
     return jsonify(errno=RET.OK, errmsg="操作成功")
+
+
+# 评论
+@news_blu.route("/news_comment", methods=["post"])
+@user_login_data
+def news_comment():
+    """评论"""
+    # 1. 获取新闻id，评论，父级评论id
+    # 2. 校验
+    # 3. 获取新闻对象
+    # 4. 创建评论对象
+    # 5. 提交sql事务，返回评论字典至前端
+    user = g.user
+    if not user:
+        return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+
+    # 1. 取到请求参数
+    news_id = request.json.get("news_id")
+    comment_content = request.json.get("comment")
+    parent_id = request.json.get("parent_id")
+
+    # 2. 判断参数
+    if not all([news_id, comment_content]):
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    try:
+        news_id = int(news_id)
+        if parent_id:
+            parent_id = int(parent_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数错误")
+
+    # 查询新闻，并判断新闻是否存在
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="数据查询错误")
+
+    if not news:
+        return jsonify(errno=RET.NODATA, errmsg="未查询到新闻数据")
+
+    # 3. 初始化一个评论模型，并且赋值
+    comment = Comment()
+    comment.user_id = user.id
+    comment.news_id = news_id
+    comment.content = comment_content
+    if parent_id:
+        comment.parent_id = parent_id
+
+    # 添加到数据库
+    # 为什么要自己去commit()?，因为在return的时候需要用到 comment 的 id
+    try:
+        db.session.add(comment)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+
+    return jsonify(errno=RET.OK, errmsg="OK", data=comment.to_dict())
+    
